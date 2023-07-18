b32831623cae38f9fef38f1e8470def315aa0693
+ <+>
2023-07-18 23:04:39 +0300

    Completed assignment

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Comment = () =>
-   <div>Naturally, I agree with this article.</div>;
-
-
+class Comment extends React.Component {
+  render() {
+   return <div>Naturally, I agree with this article.</div>;
+}
+} 
 export default Comment;
